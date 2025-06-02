88f57ed6fac03209d458bf80379b1a1b50e234cb
Lucas <lucas.da_silveira@edu.univ-fcomte.fr>
2025-06-02 10:22:03 +0200

    add Vercel Speed Insights integration to layout and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "@next/font/google";
 import LocalFont from "@next/font/local";
 import { Metadata } from "next";
 import { Analytics } from "./components/analytics";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: {
@@ -66,6 +67,7 @@ export default function RootLayout({
           }`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
