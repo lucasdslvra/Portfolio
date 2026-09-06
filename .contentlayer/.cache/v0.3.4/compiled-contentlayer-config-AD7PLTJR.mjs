0a3a50fc567526073f7018c6b4c09adb8967e624
Lucas Da Silveira <lucasdslvra@gmail.com>
2026-09-06 15:40:34 +0200

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-AD7PLTJR.mjs
+++ .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-AD7PLTJR.mjs
@@ -4,94 +4,98 @@ import remarkGfm from "remark-gfm";
 import rehypePrettyCode from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
-const computedFields = {
-	path: {
-		type: "string",
-		resolve: (doc) => `/${doc._raw.flattenedPath}`,
-	},
-	slug: {
-		type: "string",
-		resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/"),
-	},
+var computedFields = {
+  path: {
+    type: "string",
+    resolve: (doc) => `/${doc._raw.flattenedPath}`
+  },
+  slug: {
+    type: "string",
+    resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/")
+  }
 };
-const Project = defineDocumentType(() => ({
-	name: "Project",
-	filePathPattern: "./projects/**/*.mdx",
-	contentType: "mdx",
-	fields: {
-		published: {
-			type: "boolean",
-		},
-		title: {
-			type: "string",
-			required: true,
-		},
-		description: {
-			type: "string",
-			required: true,
-		},
-		date: {
-			type: "date",
-		},
-		url: {
-			type: "string",
-		},
-		repository: {
-			type: "string",
-		},
-	},
-	computedFields,
+var Project = defineDocumentType(() => ({
+  name: "Project",
+  filePathPattern: "./projects/**/*.mdx",
+  contentType: "mdx",
+  fields: {
+    published: {
+      type: "boolean"
+    },
+    title: {
+      type: "string",
+      required: true
+    },
+    description: {
+      type: "string",
+      required: true
+    },
+    date: {
+      type: "date"
+    },
+    url: {
+      type: "string"
+    },
+    repository: {
+      type: "string"
+    }
+  },
+  computedFields
 }));
-const Page = defineDocumentType(() => ({
-	name: "Page",
-	filePathPattern: "pages/**/*.mdx",
-	contentType: "mdx",
-	fields: {
-		title: {
-			type: "string",
-			required: true,
-		},
-		description: {
-			type: "string",
-		},
-	},
-	computedFields,
+var Page = defineDocumentType(() => ({
+  name: "Page",
+  filePathPattern: "pages/**/*.mdx",
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true
+    },
+    description: {
+      type: "string"
+    }
+  },
+  computedFields
 }));
-const contentlayer_config_default = makeSource({
-	contentDirPath: "./content",
-	documentTypes: [Page, Project],
-	mdx: {
-		remarkPlugins: [remarkGfm],
-		rehypePlugins: [
-			rehypeSlug,
-			[
-				rehypePrettyCode,
-				{
-					theme: "github-dark",
-					onVisitLine(node) {
-						if (node.children.length === 0) {
-							node.children = [{ type: "text", value: " " }];
-						}
-					},
-					onVisitHighlightedLine(node) {
-						node.properties.className.push("line--highlighted");
-					},
-					onVisitHighlightedWord(node) {
-						node.properties.className = ["word--highlighted"];
-					},
-				},
-			],
-			[
-				rehypeAutolinkHeadings,
-				{
-					properties: {
-						className: ["subheading-anchor"],
-						ariaLabel: "Link to section",
-					},
-				},
-			],
-		],
-	},
+var contentlayer_config_default = makeSource({
+  contentDirPath: "./content",
+  documentTypes: [Page, Project],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypePrettyCode,
+        {
+          theme: "github-dark",
+          onVisitLine(node) {
+            if (node.children.length === 0) {
+              node.children = [{ type: "text", value: " " }];
+            }
+          },
+          onVisitHighlightedLine(node) {
+            node.properties.className.push("line--highlighted");
+          },
+          onVisitHighlightedWord(node) {
+            node.properties.className = ["word--highlighted"];
+          }
+        }
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ["subheading-anchor"],
+            ariaLabel: "Link to section"
+          }
+        }
+      ]
+    ]
+  }
 });
-export { Page, Project, contentlayer_config_default as default };
+export {
+  Page,
+  Project,
+  contentlayer_config_default as default
+};
 //# sourceMappingURL=compiled-contentlayer-config-AD7PLTJR.mjs.map
